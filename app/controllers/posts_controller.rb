2311def80ed5eea550170f1265213f512b8891b0
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-01-30 13:29:57 +0545

    Updated the posts pages on the branch routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
-    def index
+    def index  
         @posts = Post.all   
     end
+
+    def show
+        # puts ">>>>> >>>> >>>> #{params[:id]}" see what paramerter is being sent through from the route
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+       
+    def create
+        post = Post.new(post_params)
+        if post.save
+            redirect_to post_path(post)           
+        else
+            redirect_to new_post_path          
+        end
+    end
+
+    private 
+        def post_params
+            params.require(:post).permit(:title,:blob,:author)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root 'home#index'
-
-  get '/posts' => 'posts#index'
+  get 'posts/index'
+  root 'posts#index'
+  post 'posts' => 'posts#create'
+  get 'posts/new' => 'posts#new', as: 'new_post'
+  get 'posts/:id' => 'posts#show' , as: 'post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
