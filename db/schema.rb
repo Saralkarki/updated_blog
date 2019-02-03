efb5d8ebc4c58bb09e7721236a42c55c3717eb38
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-02-03 09:10:45 +0545

    changed the database from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_31_083125) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "author"
