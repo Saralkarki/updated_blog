48ef9396c6a8d67b558b328e88d930e8d9a2bc94
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-02-06 19:48:21 +0545

    Made changes to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  # has_secure_password adds two additional properties to the User model. :password and :password_confirmation.
-  #Remember we only created email and password_digest when creating the model. has_secure_password works because of
-  #the gem bcrypt we have added in the gem file.
-  has_secure_password
-  # we also add validation to the fields. That validates the email, looks and the uniqueness from the database and checks if all field
-  #is entered.
-  validates  :username, :email, uniqueness: true, presence: true
-
-  has_many :post
+        # has_secure_password adds two additional properties to the User model. :password and :password_confirmation.
+        #Remember we only created email and password_digest when creating the model. has_secure_password works because of
+        #the gem bcrypt we have added in the gem file.
+        has_secure_password
+        # we also add validation to the fields. That validates the email, looks and the uniqueness from the database and checks if all field
+        #is entered.
+        validates  :username, :email, uniqueness: true, presence: true
+      
+        has_many :post
+      
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_062832) do
+ActiveRecord::Schema.define(version: 2019_02_06_135507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,14 +29,15 @@ ActiveRecord::Schema.define(version: 2019_02_04_062832) do
     t.text "blob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
   end
 
 end
