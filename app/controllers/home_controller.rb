2b0ee8762c63150184673223355c2c6acb720c1c
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-03-14 19:28:58 +0545

    removed the page number, added all the categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,10 @@ class HomeController < ApplicationController
         @post = Post.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 10)
         @post_none = Post.where(category: nil)[0..2]
         @post_coding = Post.where(category: 'Coding')[0..2]
+        @post_running = Post.where(category: 'Running')[0..2]
+        @post_books = Post.where(category: 'Books')[0..2]
+        @post_experiences = Post.where(category: 'Experiences')[0..2]
         
-               
         # @post_id = Post.find(params[:id]) 
         if session[:user_id] 
             @user_id = current_user.posts.all
